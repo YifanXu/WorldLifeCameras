ddfc50872d75a57cf5012fc980fed54bdc94d7b0
YifanXu <yifanxu00@outlook.com>
2022-07-17 15:39:26 -0700

    organize, add control buttons and desc text

---
+++ index.js
@@ -0,0 +1,106 @@
+var videoFrame = null;
+var descText = null
+var cameraId = 0;
+var cameraIntervalId = null;
+var cameraInterval = 30000;
+var documentHeight = null;
+var documentWidth = null;
+
+function SetVideoSize() {
+  var currentCamera = cameras[cameraId];
+  var width = documentWidth;
+  var height = (currentCamera.height * documentWidth / currentCamera.width).toFixed();
+  if (height > documentHeight) {
+      height = documentHeight;
+      width = (currentCamera.width * documentHeight / currentCamera.height).toFixed();
+  }
+
+  console.log("view width:", width, " view height:", height);
+
+  var widthValue = width.toString();
+  var heightValue = height.toString();
+  if ((videoFrame.width !== widthValue) || (videoFrame.height !== heightValue)) {
+      videoFrame.width = widthValue;
+      videoFrame.height = heightValue;
+  }
+}
+
+function SwitchToCamera(cameraIdToSwitch) {
+  descText.innerText = cameras[cameraIdToSwitch].Description
+  videoFrame.src = cameras[cameraIdToSwitch].url + "?autoplay=1&mute=1";
+}
+
+function SwitchToNextCamera() {
+  if (!cameraIntervalId) {
+    StartCameraLoop()
+  }
+  cameraId++;
+  if (cameraId >= cameras.length) {
+      cameraId = 0;
+  }
+  SetVideoSize();
+  SwitchToCamera(cameraId);
+}
+
+function SwitchToLastCamera() {
+  if (cameraIntervalId) {
+    stopCameraLoop()
+  }
+  cameraId--;
+  if (cameraId < 0) {
+    cameraId = cameras.length - 1;
+  }
+  SetVideoSize();
+  SwitchToCamera(cameraId);
+}
+
+function ToggleCameraLoop() {
+  if (cameraIntervalId) {
+    stopCameraLoop()
+  }
+  else {
+    StartCameraLoop()
+  }
+}
+
+function StartCameraLoop() {
+  cameraIntervalId = setInterval(SwitchToNextCamera, cameraInterval);
+}
+
+function stopCameraLoop() {
+  if (cameraIntervalId) {
+      clearInterval(cameraIntervalId);
+      cameraIntervalId = null;
+  }
+}
+
+function ResetSize() {
+  var body = document.body;
+  var html = document.documentElement;
+
+  documentHeight = Math.max(
+      document.documentElement["clientHeight"],
+      document.body["scrollHeight"],
+      document.documentElement["scrollHeight"],
+      document.body["offsetHeight"],
+      document.documentElement["offsetHeight"]
+  );
+  documentWidth = Math.max(
+      document.documentElement["clientWidth"],
+      document.body["scrollWidth"],
+      document.documentElement["scrollWidth"],
+      document.body["offsetWidth"],
+      document.documentElement["offsetWidth"]
+  );
+  SetVideoSize();
+  console.log("body width:", documentWidth, " body height:", documentHeight);
+}
+
+window.onload = function () {
+  videoFrame = document.getElementsByTagName("iframe")[0];
+  descText = document.getElementById('descText')
+  console.log(descText)
+  ResetSize();
+  SwitchToCamera(cameraId);
+  StartCameraLoop();
+}
---
+++ settings.js
@@ -0,0 +1,62 @@
+var cameraInterval = 30000;
+
+var cameras = [
+  {
+      type: "Youtube",
+      Description: "Hollywood Beach, FL Live Cam",
+      url: "https://www.youtube.com/embed/cmkAbDUEoyA",
+      width: 560,
+      height: 315,
+      source: "https://www.livebeaches.com/city/hollywood-beach-fl/"
+  },
+  {
+      type: "Youtube",
+      Description: "Hollywood Beach Band Theatre Live Cam",
+      url: "https://www.youtube.com/embed/LTqT7IRLO0c",
+      width: 560,
+      height: 315,
+      source: "https://www.livebeaches.com/webcams/hollywood-beach-band-theatre-live-cam/"
+  },
+  /*
+  {
+      type: "Youtube",
+      Description: "Oceans 13 Sports Bar & Grill Webcam Highlights",
+      url: "https://www.youtube.com/embed/IuOd2NON_vk",
+      width: 560,
+      height: 315,
+      source: "https://www.livebeaches.com/webcams/oceans-13-sports-bar-grill-webcam-highlights/"
+  },
+  */
+  {
+      type: "Youtube",
+      Description: "Oceans 13 Sports Bar & Grill Webcam Highlights",
+      url: "https://www.youtube.com/embed/bEoNVd3spEM",
+      width: 560,
+      height: 315,
+      source: "https://www.livebeaches.com/webcams/dania-beach-pier-live-cam/"
+  },
+  {
+      type: "Youtube",
+      Description: "Kiva Dunes Live Camera",
+      url: "https://www.youtube.com/embed/GvbhY-EaPJg",
+      width: 560,
+      height: 315,
+      source: "https://www.livebeaches.com/webcams/kiva-dunes-live-cam/"
+  },
+  {
+      type: "Youtube",
+      Description: "Uherský Brod Masarykovo náměstí",
+      url: "https://www.youtube.com/embed/Aq6snPCo0jM",
+      width: 560,
+      height: 315,
+      source: "https://www.webcamtaxi.com/en/czech-republic/zlin-region/masarykovo-square-cam.html"
+  },
+  {
+      type: "Youtube",
+      Description: "Live streaming di Meteo Veliero Live",
+      url: "https://www.youtube.com/embed/c0jYVsYTcfA",
+      width: 560,
+      height: 315,
+      source: "https://www.webcamtaxi.com/en/italy/veneto/venice-lidojesolo-chioscoveliero-cam.html"
+  }
+];
